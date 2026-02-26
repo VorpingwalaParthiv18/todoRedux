294b299c6398b40cf988ea2425d44cdd10a4886a
iMac <imac@iMacs-iMac.local>
2026-02-26 18:19:06 +0530

    Implement Redux for Todo management with persistence and create Todo input and list components

---
+++ store/store.ts
@@ -0,0 +1,36 @@
+import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from "../features/todo/todoslice";
+
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // localStorage
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, todoReducer);
+
+export const store = configureStore({
+  reducer: {
+    todo: persistedReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
